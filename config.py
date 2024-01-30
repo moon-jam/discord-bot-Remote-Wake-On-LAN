fa5b6b6cc71df099e9724759037f7ff547beca69
Moon Jam <wu.messi.wu@gmail.com>
2024-01-30 10:14:26 +0800

    create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# usage
+ip_address = "Your local IP" 
+port = 3389  # the  port that Microsoft remote desktop use
+mac = "Your MAC Address"
+discord_bot_token = "Your Discord Bot token"
+state_keyword = ("state")  # only lowercase
+on_keyword = ("on", "power on", "turn on", "start")  # only lowercase
